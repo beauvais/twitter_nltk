8964218aab17269e7fa5fed7cc0e90ca1b9198f7
Zach Beauvais <z.beauvais@gmail.com>
2015-07-30 21:35:07 +0100

    adds some writes to tokens_no_functions, and adds the outputs of them.

--- twitter_nltk/tokens_no_function.py
+++ twitter_nltk/tokens_no_function.py
@@ -1,13 +1,32 @@
 from nltk import *
 import pandas as pd
 
-text = "data/output.txt"
-output_file = "data/tokens.txt"
+start_file = "data/output.txt"
+token_file = "data/tokens.txt"
+word_file = "data/words.txt"
+text_file = "data/text.txt"
 
-f = open(text, 'rb')
+f = open(start_file, 'rb')
 raw = f.read()
-tokens = word_tokenize(raw.decode('utf-8'))
+tokens = wordpunct_tokenize(raw.decode('utf-8'))
 print tokens[0:20]
 
+words = [w.lower() for w in tokens]
+text = Text(tokens)
+
+token_out = open(token_file.decode('utf-8'), 'w')
+for item in tokens:
+	print>>token_out, tokens
+
+words_out = open(word_file.decode('utf-8'),'w')
+for item in words:
+	print>>words_out, words
+
+text_out = open(text_file.decode('utf-8'),'w')
+for item in text:
+	print>>text_out, text
 
 f.close()
+token_out.close()
+words_out.close()
+text_out.close()
