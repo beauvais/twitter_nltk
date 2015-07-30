6217111e908a7db037e1a0f4d0a51076cd01cad7
Zach Beauvais <z.beauvais@gmail.com>
2015-07-30 18:38:41 +0100

    updates import.py to use function

--- twitter_nltk/import.py
+++ twitter_nltk/import.py
@@ -10,10 +10,12 @@ script, filename, output_file = argv
 # Input your csv file (which, when using the twitter archive is 'tweets.csv') 
 # when calling the script. Choose your output file name at the same time.
 
-pd.set_option('display.max_colwidth', -1)
-df = pd.read_csv(filename, header=0)
-df['text'].to_csv(output_file, index=False, header=False)
+def import_text(filename):
+	pd.set_option('display.max_colwidth', -1)
+	df = pd.read_csv(filename, header=0)
+	df['text'].to_csv(output_file, index=False, header=False)
 
+import_text(filename)
 
 # We set turn off pandas' column width, because it will truncate rows if not.
 # This simply uses pandas' 'to_csv', opting not to include the index or 
