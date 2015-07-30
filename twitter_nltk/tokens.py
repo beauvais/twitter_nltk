487372333f405930abfd50ff60ab4f0ee79e5892
Zach Beauvais <z.beauvais@gmail.com>
2015-07-30 19:30:27 +0100

    adds tokens.py

---
+++ twitter_nltk/tokens.py
@@ -0,0 +1,13 @@
+from nltk import *
+
+text = "data/output.txt"
+		
+def token(text):
+	with open(text, 'rb') as f:
+		raw = f.read()
+		tokens = word_tokenize(raw.decode('utf-8'))
+		print tokens[0:20]
+		return tokens
+
+token(text)
+print tokens[0:20]
